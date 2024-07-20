01276d337d67f07f6fc8dde24a5b5bc2d05cd698
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-07-20 12:05:01 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,7 @@ const dotenv = require('dotenv');
 const errorMiddleware = require('./middleware/error');
 const path = require('path');
 
-if (process.env.NODE_ENV !== "PRODUCTION") {
-    dotenv.config({ path: "backend/config/config.env" });
-}
+require('dotenv').config();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
