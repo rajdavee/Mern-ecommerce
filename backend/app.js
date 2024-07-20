27b3f2eef6e195465e20e578439e748f6e252eac
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-07-20 13:05:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,12 @@ const dotenv = require('dotenv');
 const errorMiddleware = require('./middleware/error');
 const path = require('path');
 
+// Load environment variables from config file if not in production
 if (process.env.NODE_ENV !== "PRODUCTION") {
     dotenv.config({ path: "backend/config/config.env" });
 }
 
+// Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
@@ -22,11 +24,13 @@ const userRoutes = require('./routes/userRoute');
 const orderRoutes = require('./routes/orderRoute');
 const paymentRoutes = require('./routes/paymentRoute');
 
+// Use routes
 app.use('/api/v1', productRoutes);
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', orderRoutes);
 app.use('/api/v1', paymentRoutes);
 
+// Serve static files from the React app
 const buildPath = path.join(__dirname, "../frontend/build");
 app.use(express.static(buildPath));
 app.get("*", (req, res) => {
