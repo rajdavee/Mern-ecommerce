f5ed56380886feebe195f202a30f394d1509fade
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-07-20 14:12:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
 const errorMiddleware = require('./middleware/error');
