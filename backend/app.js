fb0da1056883053ce0b347825ce295705031c8b0
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-07-22 14:44:17 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,21 +6,15 @@ const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
 const errorMiddleware = require('./middleware/error');
 const path = require('path');
-const cors = require('cors');
 
-// Load environment variables
 if (process.env.NODE_ENV !== "PRODUCTION") {
     dotenv.config({ path: "backend/config/config.env" });
 }
 
-// Middleware setup
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(fileUpload({ useTempFiles: true, tempFileDir: '/tmp/' }));
-
-// Enable CORS
-app.use(cors({ origin: 'https://mern-project-ecommerce-raj-24642cc0.vercel.app' }));
 
 // Import routes
 const productRoutes = require('./routes/productRoute');
@@ -28,13 +22,11 @@ const userRoutes = require('./routes/userRoute');
 const orderRoutes = require('./routes/orderRoute');
 const paymentRoutes = require('./routes/paymentRoute');
 
-// Use routes
 app.use('/api/v1', productRoutes);
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', orderRoutes);
 app.use('/api/v1', paymentRoutes);
 
-// Serve frontend
 const buildPath = path.join(__dirname, "../frontend/build");
 app.use(express.static(buildPath));
 app.get("*", (req, res) => {
