59d2a70195b8344dc241417d217b3646361c5b2a
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-07-22 19:52:21 +0530

    Update NewProduct.js

--- frontend/src/component/Admin/NewProduct.js
+++ frontend/src/component/Admin/NewProduct.js
@@ -5,7 +5,7 @@ import { clearErrors, createProduct } from "../../actions/productAction";
 import { useAlert } from "react-alert";
 import { Button } from "@material-ui/core";
 import MetaData from "../layout/MetaData";
-import AccountTreeIcon from "@material-ui/icons/AccountTree";
+import AccountTree from '@mui/icons-material/AccountTree';
 import DescriptionIcon from "@material-ui/icons/Description";
 import StorageIcon from "@material-ui/icons/Storage";
 import SpellcheckIcon from "@material-ui/icons/Spellcheck";
