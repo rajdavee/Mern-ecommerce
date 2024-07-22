8b5caeefa9186381639426bcdb84e8612ac6dfd1
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-07-22 20:03:22 +0530

    Update UpdateProduct.js

--- frontend/src/component/Admin/UpdateProduct.js
+++ frontend/src/component/Admin/UpdateProduct.js
@@ -8,11 +8,11 @@ import {
 import { useAlert } from "react-alert";
 import { Button } from "@material-ui/core";
 import MetaData from "../layout/MetaData";
-import AccountTreeIcon from "@material-ui/icons/AccountTree";
-import DescriptionIcon from "@material-ui/icons/Description";
-import StorageIcon from "@material-ui/icons/Storage";
-import SpellcheckIcon from "@material-ui/icons/Spellcheck";
-import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import AccountTreeIcon from "@material-ui/AccountTree";
+import DescriptionIcon from "@material-ui/Description";
+import StorageIcon from "@material-ui/Storage";
+import SpellcheckIcon from "@material-ui/Spellcheck";
+import AttachMoneyIcon from "@material-ui/AttachMoney";
 import SideBar from "./Sidebar";
 import { UPDATE_PRODUCT_RESET } from "../../constants/productConstants";
 import { useNavigate, useParams } from "react-router-dom";
