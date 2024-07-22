0884e2017dd97473058ddd8918bcaec8ed71ed56
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-07-22 13:55:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,34 +2,33 @@ const app = require("./app");
 const cloudinary = require('cloudinary').v2;
 const connectDatabase = require("./config/database");
 
-// Handling uncaught exceptions
+
 process.on("uncaughtException", (err) => {
     console.log(`Error: ${err.message}`);
     console.log("Shutting down the server due to uncaught exceptions");
     process.exit(1);
 });
 
-// Load environment variables
+
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require('dotenv').config({ path: "backend/config/config.env" });
 }
 
-// Connect to the database
 connectDatabase();
 
-// Cloudinary configuration
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-// Start the server
-const server = app.listen(process.env.PORT || 4000, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT || 4000}`);
+
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () => {
+    console.log(`Server is working on http://localhost:${PORT}`);
 });
 
-// Handling unhandled promise rejections
+
 process.on("unhandledRejection", (err) => {
     console.log(`Error: ${err.message}`);
     console.log("Shutting down the server due to unhandled promise rejection");
