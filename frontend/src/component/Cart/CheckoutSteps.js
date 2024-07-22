b5628f20148b0f2fb90a3be5cc74337c945965d5
rajdavee <98029194+rajdavee@users.noreply.github.com>
2024-07-22 20:06:00 +0530

    Update CheckoutSteps.js

--- frontend/src/component/Cart/CheckoutSteps.js
+++ frontend/src/component/Cart/CheckoutSteps.js
@@ -1,9 +1,9 @@
 
 import React, { Fragment } from "react";
 import { Typography, Stepper, StepLabel, Step } from "@material-ui/core";
-import LocalShippingIcon from "@material-ui/icons/LocalShipping";
-import LibraryAddCheckIcon from "@material-ui/icons/LibraryAddCheck";
-import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
+import LocalShippingIcon from "@material-ui/LocalShipping";
+import LibraryAddCheckIcon from "@material-ui/LibraryAddCheck";
+import AccountBalanceIcon from "@material-ui/AccountBalance";
 import "./CheckoutSteps.css";
 
 const CheckoutSteps = ({ activeStep }) => {
